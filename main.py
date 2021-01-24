7a7b88ff2c3c1f62498a45b1ae76bffc5fff414d
CuseCose <dillemic000@gmail.com>
2021-01-24 16:03:22 -0500

    more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from graphics import *
+import numpy as np
 
 height = 800
 width = 1000
@@ -12,8 +13,26 @@ rs=10
 l=.01
 c=5*pow(10,-6)
 step=1*pow(10,-9)
-t=0
+rl=10000
 on=True
+maxtime=.1
+f=10000
+D=.5
+
+v1=0
+v2=0
+v3=0
+il=0
+ic=0
+vc=0
+for tstep in range(np.floor(maxtime/step)):
+    t=tstep*step
+    if (t%(1/f)>(1/f)*.5):
+        ''' Switch activated '''
+    else:
+        ''' Switch Off '''
+
+
 
 vouts=[]
 
